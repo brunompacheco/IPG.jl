6fa6c94efec97b5fdfdc051e91ff2f475a471b8c
Bruno Machado Pacheco <mpacheco.bruno@gmail.com>
2025-02-04 00:24:54 +0000

    refactor strategy into Player struct (with JuMP)

---
+++ src/Player.jl
@@ -0,0 +1,64 @@
+
+using JuMP
+
+abstract type AbstractPlayer end
+abstract type AbstractPayoff end
+
+struct Player <: AbstractPlayer
+    Xp::Model
+    Πp::AbstractPayoff
+    p::Integer
+end
+
+"Payoff function of player `p` with quadratic bilateral (pairwise) interactions."
+struct QuadraticPayoff <: AbstractPayoff
+    cp::Vector{Float64}
+    "`x[k]' * Qp[k] * x[p]` is the payoff component for player `p` with respect to the strategy of player `k`."
+    Qp::Vector{Matrix{Float64}}
+    function QuadraticPayoff(cp::Vector{<:Real}, Qp::Vector{<:Matrix{<:Real}})
+        if !all(Qpk->size(Qpk,2)==length(cp), Qp)
+            error("All Qp matrices must have the same number of columns as elements in cp (= dimension of p's strategy space).")
+        end
+        return new(cp, Qp)
+    end
+end
+function QuadraticPayoff(cp::Real, Qp::Vector{<:Real})
+    return QuadraticPayoff([cp], [qpk * ones(1,1) for qpk in Qp])
+end
+
+"Compute each component of the payoff of player `p` with respect to player `k`."
+function bilateral_payoff(Πp::QuadraticPayoff, p::Integer, xp::Vector{<:Union{Real,VariableRef}}, k::Integer, xk::Vector{<:Real})
+    if p == k
+        return Πp.cp' * xp - 0.5 * xp' * Πp.Qp[p] * xp
+    else
+        return xk' * Πp.Qp[k] * xp
+    end
+end
+function bilateral_payoff(player_p::Player, xp::Vector{<:Union{Real,VariableRef}}, player_k::Player, xk::Vector{<:Real})
+    return bilateral_payoff(player_p.Πp, player_p.p, xp, player_k.p, xk)
+end
+function bilateral_payoff(Πp::QuadraticPayoff, p::Integer, xp::Vector{<:Union{Real,VariableRef}}, k::Integer, σk::DiscreteMixedStrategy)
+    return expected_value(xk -> bilateral_payoff(Πp, p, xp, k, xk), σk)
+end
+function bilateral_payoff(player_p::Player, xp::Vector{<:Union{Real,VariableRef}}, player_k::Player, σk::DiscreteMixedStrategy)
+    return expected_value(xk -> bilateral_payoff(player_p.Πp, player_p.p, xp, player_k.p, xk), σk)
+end
+
+"Compute the payoff of player `p` given strategies x."
+function payoff(Πp::QuadraticPayoff, x::Vector{<:Vector{<:Real}}, p::Integer)
+    return sum([bilateral_payoff(Πp, p, x[p], k, x[k]) for k in 1:length(x)])
+end
+function payoff(Π::Vector{QuadraticPayoff}, x::Vector{<:Vector{<:Real}}, p::Integer)
+    return sum([bilateral_payoff(Π[p], p, x[p], k, x[k]) for k in 1:length(x)])
+end
+function payoff(Π::Vector{QuadraticPayoff}, σ::Vector{DiscreteMixedStrategy}, p::Integer)
+    _payoff = x -> payoff(Π, x, p)
+    return expected_value(_payoff, σ)
+end
+function payoff(player::Player, x::Vector{<:Vector{<:Real}})
+    return payoff(player.Πp, x, player.p)
+end
+function payoff(player::Player, σ::Vector{DiscreteMixedStrategy})
+    _payoff = x -> payoff(player, x)
+    return expected_value(_payoff, σ)
+end
---
+++ src/Strategies.jl
@@ -0,0 +1,47 @@
+using IterTools
+
+
+struct DiscreteMixedStrategy
+    "Probability vector."
+    probs::Vector{Float64}
+    "Support (vector of strategies)."
+    supp::Vector{Vector{Float64}}
+    function DiscreteMixedStrategy(probs::Vector{<:Real}, supp::Vector{<:Vector{<:Real}})
+        if length(probs) != size(supp, 1)
+            error("There must be as many probabilities as strategies in the support.")
+        end
+        if sum(probs) != 1
+            error("Probabilities must sum to 1.")
+        end
+        if any(probs .< 0)
+            error("Probabilities must be non-negative.")
+        end
+
+        # remove strategies with zero probability
+        return new(probs[probs .> 0], supp[probs .> 0])
+    end
+end
+
+"Compute the expected value of a function given a discrete mixed strategy."
+function expected_value(f::Function, σp::DiscreteMixedStrategy)
+    expectation = 0
+    for (prob, xp) in zip(σp.probs, σp.supp)
+        expectation += prob * f(xp)
+    end
+
+    return expectation
+end
+
+"Compute the expected value of a function given a discrete mixed profile."
+function expected_value(f::Function, σ::Vector{<:DiscreteMixedStrategy})
+    expectation = 0
+
+    # iterate over all possible _pure_ strategy profile
+    for (probs, x) in zip(product([σp.probs for σp in σ]...), product([σp.supp for σp in σ]...))
+        prob = prod(probs)
+        x = collect(x)  # convert tuple to vector
+        expectation += prob * f(x)
+    end
+
+    return expectation
+end
