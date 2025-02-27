9608c6c2cbd9949054217f4085d661fdcfd55bc3
Bruno Machado Pacheco <mpacheco.bruno@gmail.com>
2025-02-27 23:47:00 +0000

    implement generic payoff type

--- src/Game/Payoff/Payoff.jl
+++ src/Game/Payoff/Payoff.jl
@@ -1,9 +1,32 @@
 using JuMP
 
+"""
+A payoff function must implement some form of payoff computation for each player in a game.
+
+A custom implementation can have any fields, but it must implement the `payoff` function.
+The function must have the following signature:
+```julia
+function payoff(Πp::GenericPayoff, x::Vector{<:Vector{<:Union{Real,VariableRef}}}, p::Integer)
+    [...]
+end
+```
+Note that the `xp` argument can also be a vector of JuMP variable references. This is
+because the payoff functions are also used to build JuMP expressions, e.g., in
+`best_response` (see `src/Game/Player.jl`).
+"""
 abstract type AbstractPayoff end
 
-include("BilateralPayoff.jl")
+struct GenericPayoff <: AbstractPayoff
+    "The arguments must be a strategy profile and a player index, and return the player's payoff."
+    f::Function
+end
 
+"Compute the payoff of player `p` given strategies x."
+function payoff(Πp::GenericPayoff, x::Vector{<:Vector{<:Any}}, p::Integer)
+    return Πp.f(x, p)
+end
+
+include("BilateralPayoff.jl")
 
 # Utils
 
