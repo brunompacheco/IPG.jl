e300dfc168d22d67bd21af281688af25bb2a36f3
Bruno Machado Pacheco <mpacheco.bruno@gmail.com>
2025-08-08 17:25:57 +0000

    WIP: recursive replace assignment in expression

--- src/Game/Assignment.jl
+++ src/Game/Assignment.jl
@@ -7,3 +7,23 @@ Create a dictionary of variable assignments (JuMP-style) from a pure strategy.
 Assignment(player::Player, x::Vector{Float64}) = AssignmentDict(zip(all_variables(player), x))
 Assignment(x::Profile{PureStrategy})::AssignmentDict = merge(collect(Assignment(p, x_p) for (p, x_p) in x)...)
 export Assignment
+
+"Replace the variables in an expression with their assigned values."
+function replace(expr::AbstractJuMPScalar, assignment::AssignmentDict)::AbstractJuMPScalar
+    function _recursive_replace(expr::AbstractJuMPScalar)::AbstractJuMPScalar
+        if expr isa VariableRef
+            return get(assignment, expr, expr)
+        elseif expr isa AffExpr
+            # TODO: this behavior of value is unintended and may lead to problems in the future
+            return value(v -> get(assignment, v, v), expr)
+        elseif expr isa QuadExpr
+            return value(v -> get(assignment, v, v), expr)
+        elseif expr isa NonlinearExpr
+            return NonlinearExpr(expr.head, map(_recursive_replace, expr.args))
+        else
+            return expr
+        end
+    end
+
+    return _recursive_replace(expr)
+end
