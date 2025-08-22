c256dcb673be86a39d42a718dcf2b7e85ceaa602
Bruno Machado Pacheco <mpacheco.bruno@gmail.com>
2025-08-22 19:01:11 +0000

    make update_utilities not in-place anymore

--- src/SGM/UtilitiesSampledGame/Utilities.jl
+++ src/SGM/UtilitiesSampledGame/Utilities.jl
@@ -21,7 +21,7 @@ function get_utilities(players::Vector{Player}, S_X::Sample{PureStrategy})::Arra
 end
 
 "We expect the new strategies to always be the last ones in S_X."
-function update_utilities!(utilities::Array{Float64}, players::Vector{Player}, S_X::Sample{PureStrategy})
+function update_utilities(utilities::Array{Float64}, players::Vector{Player}, S_X::Sample{PureStrategy})
     for i in 1:length(players)
         n_old_p_strats = size(utilities, i)
         n_old_p_strats == length(S_X[players[i]]) && continue # no new strategies for this player
@@ -30,11 +30,14 @@ function update_utilities!(utilities::Array{Float64}, players::Vector{Player}, S
         p = players[i]
 
         # copy of S_X in which we want to have all strategies of the other players...
-        sub_S_X = Sample{PureStrategy}(k => S_X[k] for k in others(players, p))
+        sub_S_X = Sample{PureStrategy}(k => S_X[k] for k in others(players, i))
         sub_S_X[p] = S_X[p][(n_old_p_strats+1):end]  # ...and the new strategies of `p`
 
-        new_utilities = get_utilities!(players, sub_S_X)
+        new_utilities = get_utilities(players, sub_S_X)
 
         utilities = cat(utilities, new_utilities; dims=i)
     end
+
+    # it seems like concatenation is not in-place
+    return utilities
 end
