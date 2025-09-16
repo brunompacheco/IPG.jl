bf77a0800865a07ca62ee248951a5784cee71049
Bruno Machado Pacheco <mpacheco.bruno@gmail.com>
2025-09-16 19:30:49 +0000

    WIP: working for 2p games

--- examples/max-cov.jl
+++ examples/max-cov.jl
@@ -19,8 +19,11 @@ county_size = 2
 num_lakes_per_county = 50
 budget_ratio = 0.3
 
-df_edge = DataFrame(CSV.File("examples/2_50_0.3.csv"))
-info_data = load_pickle("examples/info_data.pickle")
+dirname = "EBMC_generated/$(type_dataset)_dataset/"
+fname = "$(county_size)_$(num_lakes_per_county)_$(budget_ratio).csv"
+df_edge = DataFrame(CSV.File(dirname * fname))
+
+info_data = load_pickle(dirname * "info_data.pickle")
 
 # === Unpack Experiment Settings === #
 # extract the value list for the (county_size, num_lakes_per_county, budget_ratio) key
@@ -83,26 +86,32 @@ for county in counties
     arcs_minus_c[county] = [arc for arc in arcs if (arc[1][1:2] == county) && (arc[2][1:2] != county)]
 end
 
-# === Define and Solve Game using IPG.jl === #
+# === Define and Solve SELFISH Game using IPG.jl === #
 
 using IPG, SCIP
+using IPG.JuMP: Containers
 
 # define players
 players = [Player(name=county) for county in counties]
 
 # add variables
 x_c = Dict(p => @variable(p.X, [I_c[p.name]], Bin, base_name="x_$(p.name)_") for p in players)
-y_c = Dict(p => @variable(p.X, [arcs], Bin, base_name="y_$(p.name)_") for p in players)
+y_c = Dict(p => @variable(p.X, [arcs_minus_c[p.name]], Bin, base_name="y_$(p.name)_") for p in players)
 
 # concatenate x variables
 x = Containers.DenseAxisArray(vcat([x_c[p].data for p in players]...), vcat([x_c[p].axes[1] for p in players]...))
 
-# add constraints
 for p in players
+    ### add constraints
     # TODO: x[arc[i]] may be a variable from another player; need to translate the index
-    # before using in @constraint. This is a HUGE limitation of our implementation. I'll
-    # probably need to overwrite @constraint to handle this properly. For now, I will use
-    # the private internal methods to translate VariableRef's.
-    @constraint(p.X, [arc in arcs], y_c[p][arc] <= x[arc[1]] + x[arc[2]])
+    # before using in @constraint. This is a limitation of our implementation, that I am
+    # currently handling with the internalize_expr method. Ideally, we would have the macro
+    # overwritten so that the internalization is automatic.
+    @constraint(p.X, [arc in arcs_minus_c[p.name]], y_c[p][arc] <= IPG.internalize_expr(p, x[arc[1]] + x[arc[2]]))
     @constraint(p.X, sum(x_c[p]) <= county_budget[p.name])
+
+    ### set payoff
+    set_payoff!(p, sum(t[arc] * n[arc] * y_c[p][arc] for arc in arcs_minus_c[p.name]))
 end
+
+Σ, payoff_improvements = SGM(players, SCIP.Optimizer, max_iter=10, verbose=true)
