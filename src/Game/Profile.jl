5ca2317a1bd213cf8c2088d7a8a400cd45b2416f
Bruno Machado Pacheco <mpacheco.bruno@gmail.com>
2025-09-30 20:14:02 +0000

    refactor strategy sampling from profile out of expected_value

--- src/Game/Profile.jl
+++ src/Game/Profile.jl
@@ -8,13 +8,21 @@ function expected_value(f::Function, σ::Profile{DiscreteMixedStrategy})
     expectation = 0
 
     # iterate over all possible *pure* strategy profiles
-    for (probs, x) in zip(Iterators.product([σp.probs for σp in values(σ)]...), Iterators.product([σp.supp for σp in values(σ)]...))
-        prob = prod(probs)
-        pure_profile = Dict(zip(keys(σ), x))  # create a pure profile
+    for (prob, pure_profile) in zip(probabilities(σ), support(σ))
         expectation += prob * f(pure_profile)
     end
 
     return expectation
+end
+
+function support(σ::Profile{DiscreteMixedStrategy})
+    # TODO: make this iterable? it would be nice to have lazy evaluation here...
+    return [Dict(zip(keys(σ), x)) for x in Iterators.product([σp.supp for σp in values(σ)]...)]
+end
+
+function probabilities(σ::Profile{DiscreteMixedStrategy})
+    # TODO: make this iterable? it would be nice to have lazy evaluation here...
+    return [prod(probs) for probs in Iterators.product([σp.probs for σp in values(σ)]...)]
 end
 
 function others(profile::Profile{T}, player::Player) where T <: Strategy
